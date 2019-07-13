984a512a467d7047b960b30c222b247c93bb0ccc
gilberthsolano <gilberthsolanosotomayor@gmail.com>
2019-07-13 10:42:07 -0500

    yes

--- Internas/Empresas/postulación.php
+++ Internas/Empresas/postulación.php
@@ -4,13 +4,15 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Inicio</title>
+    <title>Empresas</title>
+    <?php
+include '../../includes/estructuras/links.html';
+?>
+
+
+
+
     
-
-
-
-
-
 </head>
 <body>
     <div class="navbar-lateral full-reset">
@@ -23,23 +25,16 @@
             <div class="nav-lateral-divider full-reset"></div>
             <div class="full-reset" style="padding: 10px 0; color:#fff;">
                 <figure>
-                    <img src="../../assets/img/user.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                    <img src="../../assets/imagenes/template/user06.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
                 </figure>
                 <p class="text-center" style="padding-top: 15px;">Empresas</p>
             </div>
             <div class="nav-lateral-divider full-reset"></div>
             <div class="full-reset nav-lateral-list-menu">
                 <ul class="list-unstyled">
-                    <li><a href="../Empresas.php"><i class=></i>&nbsp;&nbsp; Resumen</a></li>
-                    <li>
-                    <li>
-                      
-                            <li><a href="#"><i class=></i>&nbsp;&nbsp; Postulacion</a></li>
-                            <li><a href="estadode[].php"><i class=></i>&nbsp;&nbsp; Informe consolidado</a></li>
-
-                       
-                    </li>
-                </ul>
+                <li><a href="#"><i class=></i>&nbsp;&nbsp; Postulación</a></li>
+                <li><a href="estadodeproyectos.php"><i class=></i>&nbsp;&nbsp; Estado de Proyectos</a></li>
+            </ul>
             </div>
         </div>
     </div>
@@ -47,10 +42,10 @@
         <nav class="navbar-user-top full-reset">
             <ul class="list-unstyled full-reset">
                 <figure>
-                   <img src="../../assets/img/user01.png" alt="user-picture" class="img-responsive img-circle center-box">
+                   <img src="../../assets/imagenes/template/user06.png" alt="user-picture" class="img-responsive img-circle center-box">
                 </figure>
                 <li style="color:#fff; cursor:default;">
-                    <span class="all-tittles">Admin Name</span>
+                    <span class="all-tittles">Postulación</span>
                 </li>
                 <li  class="tooltips-general exit-system-button" data-href="index.html" data-placement="bottom" title="Salir del sistema">
                     <i class="zmdi zmdi-power"></i>
@@ -69,202 +64,11 @@
                 </li>
             </ul>
         </nav>
-                <div class="container">
-            <div class="page-header">
-              <h1 class="all-tittles">Empresas <small>Postulacion</small></h1>
-            </div>
-        </div>
-       
-        <div class="container-fluid"  style="margin: 20px 0;">
-
-<div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Informes de Asistencias</h3>
-
-              <div class="box-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-
-                  <div class="input-group-btn">
-                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body table-responsive no-padding">
-              <table class="table table-hover">
-                <tr>
-                  <th>GP</th>
-                  <th>Paralelo</th>
-                  <th>Fecha</th>
-                  <th>Informe</th>
-                  <th>Archivo</th>
-                  <th>Validado</th>
-                  <th>Calificado</th>
-                </tr>
-                <tr>
-                  <td>2.2</td>
-                  <td>A</td>
-                  <td>12-04-2019</td>
-                  <td>Primer Bimestre</td>
-                  <td><img src="../../Assets/img/pdf.jpg" style="width: 5%;"></td>
-                  <td><span class="label label-success">Verificado</span></td>
-                  <td><span class="label label-success">Calificado</span></td>
-                </tr>
-                 <tr>
-                  <td>2.3</td>
-                  <td>B</td>
-                  <td>22-04-2019</td>
-                  <td>Primer Bimestre</td>
-                  <td><img src="../../Assets/img/pdf.jpg" style="width: 5%;"></td>
-                  <td><span class="label label-danger">Cancelado</span></td>
-                  <td><span class="label label-danger">Cancelado</span></td>
-                </tr>
-               <tr>
-                  <td>2.2</td>
-                  <td>A</td>
-                  <td>22-04-2019</td>
-                  <td>Primer Bimestre</td>
-                  <td><img src="../../Assets/img/pdf.jpg" style="width: 5%;"></td>
-                  <td><span class="label label-success">Verificado</span></td>
-                  <td><span class="label label-warning">Pendiente</span></td>
-                </tr>
-                 <tr>
-                  <td>1.3</td>
-                  <td>A</td>
-                  <td>22-04-2019</td>
-                  <td>Primer Bimestre </td>
-                  <td><img src="../../Assets/img/pdf.jpg" style="width: 5%;"></td>
-                  <td><span class="label label-warning">Pendiente</span></td>
-                  <td><span class="label label-warning">Pendiente</span></td>
-                </tr>
-               
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-      </div>
-
-
-
-        </div>
-
-       
-        <div class="modal fade " tabindex="-1" role="dialog" id="ModalHelp">
-          <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title text-center all-tittles">ayuda del sistema</h4>
-                </div>
-                <div class="modal-body ">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dignissimos qui molestias ipsum officiis unde aliquid consequatur, accusamus delectus asperiores sunt. Quibusdam veniam ipsa accusamus error. Animi mollitia corporis iusto.
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="zmdi zmdi-thumb-up"></i> &nbsp; De acuerdo</button>
-                </div>
-            </div>
-          </div>
-        </div>
-        <footer class="footer full-reset">
-            <div class="container-fluid bg-yellow">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6">
-                        <h4 class="all-tittles">Direccion:</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quam dicta et, ipsum quo. Est saepe deserunt, adipisci eos id cum, ducimus rem, dolores enim laudantium eum repudiandae temporibus sapiente.
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-6">
-                        <h4 class="all-tittles">Telefonos</h4>
-                        <ul class="list-unstyled">
-                            <li><i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; UTPL <i class="zmdi zmdi-facebook zmdi-hc-fw footer-social"></i><i class="zmdi zmdi-twitter zmdi-hc-fw footer-social"></i></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-copyright full-reset all-tittles">UTPL</div>
-        </footer>   
-    </div>
-  
-
-
-
-
-
-  
-<!-- Page script -->
-<script>
-  $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Date picker
-    $('#datepicker').datepicker({
-      autoclose: true
-    })
-
-    //iCheck for checkbox and radio inputs
-    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-      checkboxClass: 'icheckbox_minimal-blue',
-      radioClass   : 'iradio_minimal-blue'
-    })
-    //Red color scheme for iCheck
-    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-      checkboxClass: 'icheckbox_minimal-red',
-      radioClass   : 'iradio_minimal-red'
-    })
-    //Flat red color scheme for iCheck
-    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-      checkboxClass: 'icheckbox_flat-green',
-      radioClass   : 'iradio_flat-green'
-    })
-
-    //Colorpicker
-    $('.my-colorpicker1').colorpicker()
-    //color picker with addon
-    $('.my-colorpicker2').colorpicker()
-
-    //Timepicker
-    $('.timepicker').timepicker({
-      showInputs: false
-    })
-  })
-</script>
 </body>
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles">Postulación<small></small></h1>
+            </div>
+        </div>
+        
 </html>
