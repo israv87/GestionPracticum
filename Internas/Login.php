893629b80023b194f304aa3e1b54a496f9280ce6
Israel Vivanco <isra.vivanco87@gmail.com>
2020-02-19 03:53:56 -0500

    f1

--- Internas/Login.php
+++ Internas/Login.php
@@ -396,21 +396,7 @@
 
         </div>
 
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="border-top pt-5">
-              <p>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;
-                <script>
-                  document.write(new Date().getFullYear());
-                </script> All rights reserved
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
-            </div>
-          </div>
-
-        </div>
+     
       </div>
     </footer>
 
